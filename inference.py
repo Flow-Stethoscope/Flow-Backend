8ff0fb188786215f8064ac103bfeadf0d7a42e91
AniAggarwal <antiani100@gmail.com>
2021-06-12 15:31:37 -0700

    Update default path of model to new model save.

--- inference.py
+++ inference.py
@@ -9,11 +9,11 @@ from create_dataset import process_audio_np, denoise_audio_np
 
 
 class Inference:
-    def __init__(self, model_path, sample_rate):
-        # self.model = keras.models.load_model(model_path)
+    def __init__(self, sample_rate, model_path=Path("./HeartSounds_BinaryClassification_BLSTM")):
+        self.model = keras.models.load_model(model_path)
         self.sample_rate = sample_rate
 
-        # self.model.summary()
+        self.model.summary()
 
     def predict(self, byte_list, threshold=0.05):
         x = self.preprocess(byte_list)
@@ -60,6 +60,6 @@ if __name__ == "__main__":
     wav = inference.get_wav(audio_np.tolist())
     print(wav)
 
-    # inference.delete_wav(wav)
+    inference.delete_wav(wav)
 
-    # print(inference.predict(audio_np))
+    print(inference.predict(audio_np))
