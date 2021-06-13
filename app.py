fa9b026768abebb8ef92d4aa0fcbd5d494cd55ec
AniAggarwal <antiani100@gmail.com>
2021-06-12 20:24:50 -0700

    Update routes for recordings.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+
 os.system("apt install libsndfile1")
 
 import json
@@ -14,40 +15,43 @@ from schemas import User
 import ast
 
 
-client = pymongo.MongoClient("mongodb+srv://admin:mAsSeYhAcKeZ2096@flow-db.wcavd.mongodb.net/test")
+client = pymongo.MongoClient(
+    "mongodb+srv://admin:mAsSeYhAcKeZ2096@flow-db.wcavd.mongodb.net/test"
+)
 db = client["flow"]
 inference = Inference()
 
 # files = request.files
 
 
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp3'}
+ALLOWED_EXTENSIONS = {"txt", "pdf", "png", "jpg", "jpeg", "gif", "mp3"}
 app = Flask(__name__)
 app.run()
 
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
     return {"Health": "Good"}
 
 
-@app.route('/test', methods=['GET'])
+@app.route("/test", methods=["GET"])
 def test():
     return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
 
-@app.route('/register_user', methods=['POST'])
+@app.route("/register_user", methods=["POST"])
 def register_user():
     db_user = request.form.to_dict()
     if User.user.is_valid(db_user):
-        if (db["users"].find_one({'username': db_user["username"]})) is not None:
+        if (db["users"].find_one({"username": db_user["username"]})) is not None:
             return "This username already exists"
-        db_user["password"] = bcrypt.hashpw(db_user["password"].encode('utf-8'), bcrypt.gensalt())
+        db_user["password"] = bcrypt.hashpw(
+            db_user["password"].encode("utf-8"), bcrypt.gensalt()
+        )
         db_user["recordings"] = []
         if db_user["user_type"] == "Patient":
             db_user["doctors"] = []
@@ -60,15 +64,19 @@ def register_user():
     return "Form data incorrectly formatted"
 
 
-@app.route('/login/<username>', methods=['POST'])
+@app.route("/login/<username>", methods=["POST"])
 def login(username):
-    return str(bcrypt.checkpw(request.form.to_dict()["password"].encode('utf-8'),
-                              db["users"].find_one({"username": username})["password"]))
+    return str(
+        bcrypt.checkpw(
+            request.form.to_dict()["password"].encode("utf-8"),
+            db["users"].find_one({"username": username})["password"],
+        )
+    )
 
 
-@app.route('/users/<username>', methods=['GET'])
+@app.route("/users/<username>", methods=["GET"])
 def user(username):
-    user_info = (db["users"].find_one({"username": username}))
+    user_info = db["users"].find_one({"username": username})
     user_info["_id"] = None
     user_info["password"] = None
     print(user_info)
@@ -76,7 +84,7 @@ def user(username):
 
 
 # client sends bytes to backend
-@app.route('/send_recording', methods=['POST'])
+@app.route("/send_recording", methods=["POST"])
 def send_recording():
     recording = request.form.to_dict()
     bytes = recording["recording"]
@@ -90,31 +98,30 @@ def send_recording():
     inference.delete_wav(path)
     # if Recording.recording.is_valid(recording):
     if True:
-        rec_id = db["recordings"].insert_one({
-            "url": url,
-            "username": recording["username"],
-            "classification": classification
-        }).inserted_id
+        rec_id = (
+            db["recordings"]
+            .insert_one(
+                {
+                    "url": url,
+                    "username": recording["username"],
+                    "classification": classification,
+                }
+            )
+            .inserted_id
+        )
         db.patients.update(
-            {
-                "username": recording["username"]
-            },
+            {"username": recording["username"]},
             {
                 "$push": {
-                    "recordings":
-                        db["recordings"].find_one(
-                            {"_id": rec_id}
-                        )["_id"]
+                    "recordings": db["recordings"].find_one({"_id": rec_id})["_id"]
                 }
-            }
+            },
         )
-    return "True"
+    return {"recording_id": rec_id}
 
 
-@app.route('/recordings/<id>', methods=['GET'])
+@app.route("/recordings/<id>", methods=["GET"])
 def recording(id):
     return db["recordings"].find_one(
-        {
-            "_id": id
-        }
+        {"_id": id}, {"_id": 1, "url": 1, "classification": 1}
     )
